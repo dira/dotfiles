8fbb852dc94d064993b9d22b4ea32786fe911dcb
Irina Dumitrascu <me@dira.ro>
2011-11-07 12:35:32 +0100

    Aliases for db migrate and rollback

--- .oh-my-zsh/custom/dira.zsh
+++ .oh-my-zsh/custom/dira.zsh
@@ -13,3 +13,8 @@ alias rti='rake test:integration'
 alias r='./script/rails'
 alias rc='r c'
 alias rgm='r g migration'
+
+alias rdm='rake db:migrate'
+alias rdmt='rake db:migrate RAILS_ENV=test'
+alias rdr='rake db:rollback'
+alias rdrt='rake db:rollback RAILS_ENV=test'
